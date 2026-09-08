41cc83cee6fd2a4d294ec1a7ab0af7117f8365b0
einekuhmachtmuh <c44986054@gmail.com>
2026-09-08 21:27:36 +0800

    fix: align JNI runtime API declarations

--- c/jni/ck_jni_runtime.h
+++ c/jni/ck_jni_runtime.h
@@ -25,7 +25,8 @@ struct ck_runtime
 typedef struct ck_runtime ck_runtime_t;
 
 int ck_runtime_init(ck_runtime_t *runtime, const char *class_path);
-int ck_runtime_dispatch_smoke(ck_runtime_t *runtime, ck_request_t *request);
+int ck_runtime_dispatch_async_smoke(ck_runtime_t *runtime, ck_request_t *request);
+int ck_runtime_poll_completion(ck_runtime_t *runtime, ck_request_t *request);
 int ck_runtime_shutdown(ck_runtime_t *runtime);
 void ck_runtime_destroy(ck_runtime_t *runtime);
 

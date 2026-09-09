bf59aab3c6727558004dfeaf784ffe8aefa97112
einekuhmachtmuh <c44986054@gmail.com>
2026-09-09 21:31:01 +0800

    feat: add connection-owned HTTP read consumer

---
+++ c/http/ck_http_connection_reader.h
@@ -0,0 +1,47 @@
+#ifndef CKARTA_HTTP_CONNECTION_READER_H
+#define CKARTA_HTTP_CONNECTION_READER_H
+
+#include <stddef.h>
+
+#include "ck_http_input.h"
+
+#define CK_HTTP_CONNECTION_READ_BUFFER_BYTES 65536u
+
+typedef enum ck_http_connection_read_result
+{
+	CK_HTTP_CONNECTION_READ_INCOMPLETE = 0,
+	CK_HTTP_CONNECTION_READ_REQUEST_COMPLETE = 1,
+	CK_HTTP_CONNECTION_READ_BAD_REQUEST = 2,
+	CK_HTTP_CONNECTION_READ_TOO_LARGE = 3,
+	CK_HTTP_CONNECTION_READ_EOF_INCOMPLETE = 4,
+	CK_HTTP_CONNECTION_READ_IO_ERROR = 5,
+	CK_HTTP_CONNECTION_READ_SINK_ERROR = 6
+} ck_http_connection_read_result_t;
+
+typedef int (*ck_http_body_sink_fn)(
+	void *context,
+	const unsigned char *data,
+	size_t length);
+
+typedef struct ck_http_connection_reader
+{
+	ck_http_input_t input;
+	unsigned char buffer[CK_HTTP_CONNECTION_READ_BUFFER_BYTES];
+	size_t begin;
+	size_t end;
+} ck_http_connection_reader_t;
+
+void ck_http_connection_reader_init(ck_http_connection_reader_t *reader);
+ck_http_connection_read_result_t ck_http_connection_reader_drive(
+	ck_http_connection_reader_t *reader,
+	int socket_fd,
+	ck_http_body_sink_fn body_sink,
+	void *body_sink_context);
+void ck_http_connection_reader_next_request(
+	ck_http_connection_reader_t *reader);
+const ck_http_request_t *ck_http_connection_reader_request(
+	const ck_http_connection_reader_t *reader);
+size_t ck_http_connection_reader_buffered_bytes(
+	const ck_http_connection_reader_t *reader);
+
+#endif

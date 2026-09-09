3cafc5ce0737ab2ad1905240965e9ea24a677ade
einekuhmachtmuh <c44986054@gmail.com>
2026-09-10 07:28:40 +0800

    feat: expose transactional body backpressure in HTTP reader

--- c/http/ck_http_connection_reader.h
+++ c/http/ck_http_connection_reader.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 #include "ck_http_input.h"
+#include "ck_http_request_body.h"
 
 #define CK_HTTP_CONNECTION_READ_BUFFER_BYTES 65536u
 #define CK_HTTP_CONNECTION_READ_BUDGET_BYTES 32768u
@@ -17,9 +18,12 @@ typedef enum ck_http_connection_read_result
 	CK_HTTP_CONNECTION_READ_TOO_LARGE = 3,
 	CK_HTTP_CONNECTION_READ_EOF_INCOMPLETE = 4,
 	CK_HTTP_CONNECTION_READ_IO_ERROR = 5,
-	CK_HTTP_CONNECTION_READ_SINK_ERROR = 6
+	CK_HTTP_CONNECTION_READ_SINK_ERROR = 6,
+	CK_HTTP_CONNECTION_READ_BODY_BACKPRESSURE = 7
 } ck_http_connection_read_result_t;
 
+
+#define CK_HTTP_BODY_SINK_WOULD_BLOCK 2
 
 typedef int (*ck_http_body_sink_fn)(
 	void *context,
@@ -29,12 +33,16 @@ typedef int (*ck_http_body_sink_fn)(
 typedef struct ck_http_connection_reader
 {
 	ck_http_input_t input;
+	ck_http_request_body_t *body_queue;
 	unsigned char buffer[CK_HTTP_CONNECTION_READ_BUFFER_BYTES];
 	size_t begin;
 	size_t end;
 } ck_http_connection_reader_t;
 
 void ck_http_connection_reader_init(ck_http_connection_reader_t *reader);
+void ck_http_connection_reader_attach_body_queue(
+	ck_http_connection_reader_t *reader,
+	ck_http_request_body_t *body_queue);
 ck_http_connection_read_result_t ck_http_connection_reader_drive(
 	ck_http_connection_reader_t *reader,
 	int socket_fd,

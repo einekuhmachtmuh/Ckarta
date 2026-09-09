f82cd8156b9c973215107a2b3b0713376f186856
einekuhmachtmuh <c44986054@gmail.com>
2026-09-10 07:25:13 +0800

    feat: add direct io_uring capability probe

---
+++ c/event/ck_io_uring_probe.h
@@ -0,0 +1,22 @@
+#ifndef CKARTA_IO_URING_PROBE_H
+#define CKARTA_IO_URING_PROBE_H
+
+#include <stdint.h>
+
+typedef struct ck_io_uring_probe_result
+{
+	int available;
+	int error_number;
+	uint32_t features;
+	int fast_poll;
+	int accept;
+	int recv;
+	int send;
+	int accept_multishot;
+	int recv_multishot;
+	int provided_buffer_ring;
+} ck_io_uring_probe_result_t;
+
+int ck_io_uring_probe(ck_io_uring_probe_result_t *result);
+
+#endif

eb6a769b1ddbe22b6f576b77d3ce889395e8417b
einekuhmachtmuh <c44986054@gmail.com>
2026-09-09 21:22:49 +0800

    test: integrate HTTP input state with TCP consumer

--- tests/net/ck_tcp_event_integration_test.c
+++ tests/net/ck_tcp_event_integration_test.c
@@ -2,7 +2,7 @@
 
 #include "../../c/connection/ck_connection_registry.h"
 #include "../../c/event/ck_event_loop.h"
-#include "../../c/http/ck_http_parser.h"
+#include "../../c/http/ck_http_input.h"
 #include "../../c/net/ck_tcp_listener.h"
 
 #include <assert.h>
@@ -35,18 +35,33 @@ int main(void)
 	ck_tcp_listener_t listener = {0};
 	ck_event_loop_t loop = {0};
 	ck_connection_registry_t registry = {0};
-	ck_http_parser_t parser = {0};
-	ck_http_request_t parsed_request = {0};
+	ck_http_input_t http_input;
+	ck_http_input_t next_input;
+	ck_http_input_result_t http_result;
 	ck_event_notification_t notifications[4] = {0};
 	ck_connection_handle_t handle;
 	ck_connection_handle_t reused_handle;
-	const char payload[] = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+	const char payload[] =
+		"POST /upload HTTP/1.1\r\n"
+		"Host: localhost\r\n"
+		"Content-Length: 5\r\n"
+		"\r\n"
+		"hello"
+		"GET /next HTTP/1.1\r\n"
+		"Host: localhost\r\n\r\n";
 	const size_t payload_length = sizeof(payload) - 1U;
-	char request[sizeof(payload)];
+	char received[512];
 	int client_fd;
 	int accepted_fd;
 	int count;
 	size_t consumed;
+	size_t body_length;
+	size_t total_received = 0;
+	size_t first_request_end = 0;
+	const unsigned char *body;
+
+	ck_http_input_init(&http_input);
+	ck_http_input_init(&next_input);
 
 	assert(ck_tcp_listener_init(&listener, 0) == 0);
 	assert(ck_event_loop_init(&loop) == 0);
@@ -79,19 +94,58 @@ int main(void)
 
 	assert(ck_connection_registry_socket_fd(&registry, notifications[0].cookie,
 		UINT64_C(2001), UINT64_C(3001), UINT64_C(4001)) == accepted_fd);
-	assert(recv(accepted_fd, request, sizeof(request), MSG_DONTWAIT)
-			== (ssize_t)payload_length);
-	assert(memcmp(request, payload, payload_length) == 0);
-	ck_http_parser_init(&parser);
-	assert(ck_http_parser_feed(&parser, request, payload_length,
-		&consumed, &parsed_request) == CK_HTTP_PARSE_COMPLETE);
-	assert(consumed == payload_length);
-	assert(parsed_request.method.length == 3);
-	assert(memcmp(parsed_request.method.data, "GET", 3) == 0);
-	assert(parsed_request.target.length == 1);
-	assert(memcmp(parsed_request.target.data, "/", 1) == 0);
-	assert(parsed_request.body_mode == CK_HTTP_BODY_NONE);
-	assert(recv(accepted_fd, request, sizeof(request), MSG_DONTWAIT) == -1);
+
+	for (;;)
+	{
+		ssize_t received_now = recv(accepted_fd, received + total_received,
+			sizeof(received) - total_received, MSG_DONTWAIT);
+		if (received_now < 0)
+		{
+			assert(errno == EAGAIN || errno == EWOULDBLOCK);
+			break;
+		}
+		assert(received_now > 0);
+		http_result = ck_http_input_feed(&http_input,
+			received + total_received, (size_t)received_now,
+			&consumed, &body, &body_length);
+		assert(consumed <= (size_t)received_now);
+		if (body_length != 0)
+		{
+			assert(body_length <= 5);
+			assert(total_received + consumed >= payload_length - strlen("GET /next HTTP/1.1\r\nHost: localhost\r\n\r\n"));
+		}
+		total_received += (size_t)received_now;
+		if (http_result == CK_HTTP_INPUT_COMPLETE)
+		{
+			first_request_end = total_received - (size_t)received_now + consumed;
+			break;
+		}
+		assert(http_result == CK_HTTP_INPUT_INCOMPLETE
+				|| http_result == CK_HTTP_INPUT_BODY);
+		assert(total_received < sizeof(received));
+	}
+
+	assert(first_request_end != 0);
+	assert(first_request_end < total_received);
+	assert(total_received <= payload_length);
+	assert(memcmp(received, payload, total_received) == 0);
+	assert(memcmp(received + first_request_end,
+			"GET /next HTTP/1.1\r\nHost: localhost\r\n\r\n",
+			strlen("GET /next HTTP/1.1\r\nHost: localhost\r\n\r\n")) == 0);
+
+	assert(ck_http_input_next_request, "next request reset API exists");
+	ck_http_input_next_request(&next_input);
+	http_result = ck_http_input_feed(&next_input,
+		received + first_request_end,
+		total_received - first_request_end,
+		&consumed, &body, &body_length);
+	assert(http_result == CK_HTTP_INPUT_COMPLETE);
+	assert(consumed == total_received - first_request_end);
+	assert(body_length == 0);
+	assert(next_input.request.target.length == 5);
+	assert(memcmp(next_input.request.target.data, "/next", 5) == 0);
+
+	assert(recv(accepted_fd, received, sizeof(received), MSG_DONTWAIT) == -1);
 	assert(errno == EAGAIN || errno == EWOULDBLOCK);
 
 	assert(close(client_fd) == 0);

c31e79692fbeec797e18a07a33591ba7535c7b7e
einekuhmachtmuh <c44986054@gmail.com>
2026-09-09 19:50:38 +0800

    test: verify native connection owns and closes its socket

--- tests/connection/ck_connection_test.c
+++ tests/connection/ck_connection_test.c
@@ -2,6 +2,8 @@
 
 #include <assert.h>
 #include <pthread.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 struct race_args
 {
@@ -24,9 +26,16 @@ int main(void)
 	struct race_args disconnect = { 0 };
 	pthread_t complete_thread;
 	pthread_t disconnect_thread;
+	int socket_pair[2];
+	char byte;
 	int winners;
 
+	assert(socketpair(AF_UNIX, SOCK_STREAM, 0, socket_pair) == 0);
 	assert(ck_connection_init(&connection, 1, 11, 22, 33) == 0);
+	assert(ck_connection_socket_fd(&connection) == -1);
+	assert(ck_connection_attach_socket(&connection, socket_pair[0]) == 0);
+	assert(ck_connection_socket_fd(&connection) == socket_pair[0]);
+	assert(ck_connection_attach_socket(&connection, socket_pair[1]) == 1);
 	assert(ck_connection_validate(&connection, 11, 22, 33) == 0);
 	assert(ck_connection_validate(&connection, 11, 22, 34) == 1);
 	assert(ck_connection_start_async_cycle(&connection, 42) == 0);
@@ -45,8 +54,11 @@ int main(void)
 			&connection, CK_CONNECTION_TERMINAL_TIMEOUT)
 			== CK_CONNECTION_TERMINAL_ALREADY_DIFFERENT);
 	assert(ck_connection_close(&connection) == 0);
+	assert(ck_connection_socket_fd(&connection) == -1);
 	assert(ck_connection_close(&connection) == 1);
 	assert(ck_connection_state(&connection) == CK_CONNECTION_CLOSED);
+	assert(recv(socket_pair[1], &byte, 1, 0) == 0);
+	assert(close(socket_pair[1]) == 0);
 
 	assert(ck_connection_init(&connection, 2, 21, 22, 23) == 0);
 	assert(ck_connection_start_async(&connection) == 0);

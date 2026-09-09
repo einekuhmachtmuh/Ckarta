361195c94c96b174c168489014d3a0e2533fccce
einekuhmachtmuh <c44986054@gmail.com>
2026-09-09 21:42:30 +0800

    test: cover fragmented HTTP chunk delimiter

--- tests/http/ck_http_connection_reader_test.c
+++ tests/http/ck_http_connection_reader_test.c
@@ -117,6 +117,49 @@ static void test_chunked_stream(void)
 	assert(close(sockets[1]) == 0);
 }
 
+static void test_chunked_split_crlf(void)
+{
+	static const char first[] =
+		"POST /split HTTP/1.1\r\n"
+		"Transfer-Encoding: chunked\r\n"
+		"\r\n"
+		"4\r\nWiki\r";
+	static const char second[] =
+		"\n"
+		"0\r\n\r\n";
+	ck_http_connection_reader_t reader;
+	body_capture_t capture = {0};
+	int sockets[2];
+	ck_http_connection_read_result_t result;
+	const ck_http_request_t *request;
+
+	assert(socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) == 0);
+	ck_http_connection_reader_init(&reader);
+
+	assert(send(sockets[0], first, sizeof(first) - 1U, 0)
+			== (ssize_t)(sizeof(first) - 1U));
+	result = ck_http_connection_reader_drive(&reader, sockets[1],
+			capture_body, &capture);
+	assert(result == CK_HTTP_CONNECTION_READ_INCOMPLETE);
+	assert(capture.length == 4);
+	assert(memcmp(capture.data, "Wiki", 4) == 0);
+
+	assert(send(sockets[0], second, sizeof(second) - 1U, 0)
+			== (ssize_t)(sizeof(second) - 1U));
+	result = ck_http_connection_reader_drive(&reader, sockets[1],
+			capture_body, &capture);
+	assert(result == CK_HTTP_CONNECTION_READ_REQUEST_COMPLETE);
+	request = ck_http_connection_reader_request(&reader);
+	assert(request != NULL);
+	assert(request->target.length == 6);
+	assert(memcmp(request->target.data, "/split", 6) == 0);
+	assert(capture.length == 4);
+	assert(memcmp(capture.data, "Wiki", 4) == 0);
+
+	assert(close(sockets[0]) == 0);
+	assert(close(sockets[1]) == 0);
+}
+
 static void test_eof_incomplete_body(void)
 {
 	static const char header[] =
@@ -145,6 +188,7 @@ int main(void)
 {
 	test_content_length_pipeline();
 	test_chunked_stream();
+	test_chunked_split_crlf();
 	test_eof_incomplete_body();
 	return 0;
 }

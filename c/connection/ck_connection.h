237a80f2dddbeb3a99a2ba9b4c3fc38ea9bcff62
einekuhmachtmuh <c44986054@gmail.com>
2026-09-09 17:57:18 +0800

    feat: add connection ownership state machine

---
+++ c/connection/ck_connection.h
@@ -0,0 +1,52 @@
+#ifndef CKARTA_CONNECTION_H
+#define CKARTA_CONNECTION_H
+
+#include <stdatomic.h>
+#include <stdint.h>
+
+typedef enum ck_connection_state
+{
+	CK_CONNECTION_OPEN = 0,
+	CK_CONNECTION_ASYNC_WAIT = 1,
+	CK_CONNECTION_CLOSING = 2,
+	CK_CONNECTION_CLOSED = 3,
+	CK_CONNECTION_STATE_INVALID = -1
+} ck_connection_state_t;
+
+typedef enum ck_connection_terminal_event
+{
+	CK_CONNECTION_TERMINAL_COMPLETE = 0,
+	CK_CONNECTION_TERMINAL_CLIENT_DISCONNECT = 1,
+	CK_CONNECTION_TERMINAL_TIMEOUT = 2,
+	CK_CONNECTION_TERMINAL_ERROR = 3,
+	CK_CONNECTION_TERMINAL_SHUTDOWN = 4
+} ck_connection_terminal_event_t;
+
+typedef struct ck_connection
+{
+	uint64_t connection_id;
+	uint64_t request_id;
+	uint64_t owner_token;
+	uint64_t lifetime_token;
+	_Atomic uint32_t state;
+	_Atomic int32_t terminal_event;
+} ck_connection_t;
+
+int ck_connection_init(ck_connection_t *connection,
+		uint64_t connection_id,
+		uint64_t request_id,
+		uint64_t owner_token,
+		uint64_t lifetime_token);
+int ck_connection_start_async(ck_connection_t *connection);
+int ck_connection_try_terminal(ck_connection_t *connection,
+		ck_connection_terminal_event_t event);
+int ck_connection_close(ck_connection_t *connection);
+int ck_connection_validate(const ck_connection_t *connection,
+		uint64_t request_id,
+		uint64_t owner_token,
+		uint64_t lifetime_token);
+ck_connection_state_t ck_connection_state(const ck_connection_t *connection);
+ck_connection_terminal_event_t ck_connection_terminal_event(
+		const ck_connection_t *connection);
+
+#endif

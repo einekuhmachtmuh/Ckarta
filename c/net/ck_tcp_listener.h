91dd37083d5e513b032a4161c8edacd9cf21d7b4
einekuhmachtmuh <c44986054@gmail.com>
2026-09-10 09:33:44 +0800

    revert: restore listener fd API baseline

--- c/net/ck_tcp_listener.h
+++ c/net/ck_tcp_listener.h
@@ -1,19 +1,16 @@
 #ifndef CKARTA_TCP_LISTENER_H
 #define CKARTA_TCP_LISTENER_H
 
-#include "../platform/ck_socket.h"
-
 typedef struct ck_tcp_listener
 {
-	ck_socket_t socket;
+	int socket_fd;
 	int initialized;
 } ck_tcp_listener_t;
 
 int ck_tcp_listener_init(ck_tcp_listener_t *listener, unsigned short port);
 int ck_tcp_listener_port(const ck_tcp_listener_t *listener);
-ck_socket_t ck_tcp_listener_socket(const ck_tcp_listener_t *listener);
-int ck_tcp_listener_accept(ck_tcp_listener_t *listener,
-	ck_socket_t *accepted_socket);
+int ck_tcp_listener_fd(const ck_tcp_listener_t *listener);
+int ck_tcp_listener_accept(ck_tcp_listener_t *listener);
 int ck_tcp_listener_destroy(ck_tcp_listener_t *listener);
 
 #endif
